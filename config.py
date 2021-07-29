2dd84f0c6c6cd9d272aa6af2981ccae99205698a
Shamil Habeeb <shamilhabeeb5@gmail.com>
2021-07-29 12:54:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import re
 from youtube_dl import YoutubeDL
 
 class Config:
-    API_ID = int(os.environ.get("API_ID", ''))
+    APP_ID = int(os.environ.get("APP_ID", ''))
     API_HASH = os.environ.get("API_HASH", "")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
     DOWNLOAD_LOCATION = os.environ.get("DOWNLOAD_LOCATION", "./DOWNLOADS/")
