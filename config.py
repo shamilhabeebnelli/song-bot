47ab66ff398dae4e728016af88da412da27ca7a7
Shamil Habeeb <shamilhabeeb5@gmail.com>
2021-07-31 14:15:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,6 +6,8 @@ class Config:
     APP_ID = int(os.environ.get("APP_ID", ''))
     API_HASH = os.environ.get("API_HASH", "")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-    OWNER = os.environ.get("OWNER", "") 
+    START_MSG = os.environ.get("START_MSG", "<b>Hi {},\n Iam A Simple Youtube to Mp3 Downloader Bot,</b>\n\nSend me Any Songs name or youtube links with /song command")
+    START_IMG = os.environ.get("START_IMG", "https://telegra.ph/file/2a35fca576aa49de77c98.jpg")
+    OWNER = os.environ.get("OWNER", "shamilhabeeb") 
     DOWNLOAD_LOCATION = os.environ.get("DOWNLOAD_LOCATION", "./DOWNLOADS/")
     msg = {}
