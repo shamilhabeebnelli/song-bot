9415d389db1a04a940bef40faf30ef739fb69550
Shamil Habeeb <shamilhabeeb5@gmail.com>
2021-10-03 12:57:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -16,10 +16,10 @@ logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
 
 app = pyrogram.Client(
-      "Siri",
+      "mwk",
        bot_token=Config.BOT_TOKEN,
        api_id=Config.APP_ID,
        api_hash=Config.API_HASH,
-       plugins=dict(root="siri")
+       plugins=dict(root="modules")
     )
 app.run()
